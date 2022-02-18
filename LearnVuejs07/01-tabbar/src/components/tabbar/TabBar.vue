bd41d98cbc52a6a9f318b4563babd9f59a63d6d0
Erin-hua <wyinghua939@gmail.com>
2022-02-18 20:02:09 +0800

    0.3

---
+++ LearnVuejs07/01-tabbar/src/components/tabbar/TabBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="tab-bar">
+    <!-- <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/home.svg" alt="">
+      <div>首页</div>
+    </div>
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/home.svg" alt="">
+      <div>分类</div>
+    </div>
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/home.svg" alt="">
+      <div>购物车</div>
+    </div>
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/home.svg" alt="">
+      <div>我的</div>
+    </div> -->
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar"
+};
+</script>
+
+<style>
+  #tab-bar {
+    display: flex;
+    background-color: #f6f6f6;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.2);
+  }
+</style>
